4a3f444e6a338df94b782264b25ce1259a4bd0a1
Saiharshith Eedara <142712608+Saiharshith17@users.noreply.github.com>
2025-02-16 23:52:04 +0530

    Update FoodItem.js

--- src/FoodItem/FoodItem.js
+++ src/FoodItem/FoodItem.js
@@ -8,9 +8,6 @@ import { StoreContext } from '../context/StoreContext';
 
 function FoodItem({id,name,price,description,image}) {
 
-
-
-    
     const{cartItems,addToCart,removeFromCart}=useContext(StoreContext);
   
   return (
